0eed81b600b879778c2b606792584149a115237b
Hyunmaa <fredley.infor@gmail.com>
2024-12-16 18:51:38 +0700

    Create ticket with Joi validation

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -1,5 +1,47 @@
+// const { Sequelize, DataTypes } = require("sequelize");
+// const { db } = require("../config/config.json");
+// const sequelize = new Sequelize(
+//   db.database,
+//   db.username,
+//   db.password,
+//   {
+//     host: db.host,
+//     port: 3306,
+//     dialect: "mysql",
+//   }
+// );
+const Joi = require("joi");
+
+//=================================================================
+
 async function create(req, res) {
-  return res.status(200).send({ message: 'POST /ticket' });
+  const {ticketTitle, ticketPrice, ticketDescription} = req.body;
+  const schema = Joi.object({
+    ticketTitle: Joi.string().required().messages({
+      'string.empty': '"Nama tiket wajib diisi"',
+      'any.required': '"Nama tiket wajib diisi"'
+    }),
+    ticketPrice: Joi.number().min(0).max(100000000).required().messages({
+      'number.base': '"Harga tiket wajib diisi menggunakan angka"',
+      'number.min': '"Minimal harga tiket 0"',
+      'number.max': '"Maximal harga tiket 100000000"',
+      'number.empty': `"Harga tiket wajib diisi"`,
+      'any.required': '"Harga tiket wajib diisi"'
+    }),
+    ticketDescription: Joi.string().required().messages({
+      'string.empty': '"Deksripsi wajib diisi"',
+      'any.required': '"Deksripsi wajib diisi"'
+    })
+  });
+  //Schema Validation
+  try{
+    await schema.validateAsync(req.body);
+  }
+  catch(error){
+    return res.status(403).send(error.toString());
+  }
+
+  // return res.status(200).send({ message: 'POST /ticket' });
 }
 
 async function getAll(req, res) {
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,16 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const { db } = require("../config/config.json");
-const sequelize = new Sequelize(
-  db.database,
-  db.username,
-  db.password,
-  {
-    host: db.host,
-    port: 3306,
-    dialect: "mysql",
-  }
-);
-const Joi = require("joi");
+// const { Sequelize, DataTypes } = require("sequelize");
+// const { db } = require("../config/config.json");
+// const sequelize = new Sequelize(
+//   db.database,
+//   db.username,
+//   db.password,
+//   {
+//     host: db.host,
+//     port: 3306,
+//     dialect: "mysql",
+//   }
+// );
+// const Joi = require("joi");
 
 async function create(req, res) {
   return res.status(200).send({ message: 'POST /user' });
@@ -22,28 +22,6 @@ async function updateUser(req, res) {
 
 async function getAll(req, res) {
   return res.status(200).send({ message: 'GET /user' });
-}
-
-//============================================================
-
-const register = async(req, res) => {
-  const { username, password, email } = req.body;
-  const schema = Joi.object({
-    username: Joi.string().required().messages({
-        "any.required": "Field tidak boleh kosong",
-        "string.empty": "Field tidak boleh kosong"
-    }),
-    password: Joi.string().required().min(6).messages({
-        "any.required": "Field tidak boleh kosong",
-        "string.empty": "Field tidak boleh kosong",
-        "string.min": "Password minimal 6 karakter"
-    }),
-    email: Joi.string().email().required().messages({
-        "any.required": "Field tidak boleh kosong",
-        "string.empty": "Field tidak boleh kosong",
-        "string.email": "Format email salah"
-    })
-  });
 }
 
 module.exports = {
