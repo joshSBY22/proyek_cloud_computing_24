a2e574175e2d7eb2ce1bc10e97a51858ec7ec3f5
Hyunmaa <fredley.infor@gmail.com>
2024-12-16 17:58:51 +0700

    Test

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,53 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { db } = require("../config/config.json");
+const sequelize = new Sequelize(
+  db.database,
+  db.username,
+  db.password,
+  {
+    host: db.host,
+    port: 3306,
+    dialect: "mysql",
+  }
+);
+const Joi = require("joi");
+
 async function create(req, res) {
   return res.status(200).send({ message: 'POST /user' });
+}
+
+async function updateUser(req, res) {
+  return res.status(200).send({ message: `POST /user` });
 }
 
 async function getAll(req, res) {
   return res.status(200).send({ message: 'GET /user' });
 }
 
+//============================================================
+
+const register = async(req, res) => {
+  const { username, password, email } = req.body;
+  const schema = Joi.object({
+    username: Joi.string().required().messages({
+        "any.required": "Field tidak boleh kosong",
+        "string.empty": "Field tidak boleh kosong"
+    }),
+    password: Joi.string().required().min(6).messages({
+        "any.required": "Field tidak boleh kosong",
+        "string.empty": "Field tidak boleh kosong",
+        "string.min": "Password minimal 6 karakter"
+    }),
+    email: Joi.string().email().required().messages({
+        "any.required": "Field tidak boleh kosong",
+        "string.empty": "Field tidak boleh kosong",
+        "string.email": "Format email salah"
+    })
+  });
+}
+
 module.exports = {
   create,
-  getAll
+  getAll,
+  updateUser
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/user.controller');
 
 userRouter.get('/', userController.getAll);
 
+userRouter.post('/:id', userController.updateUser);
+
 userRouter.post('/', userController.create);
 
 module.exports = userRouter;
